9289da3b72ea0878305308e208d34db6e84bc0ef
Cameron Roman <camroman95@gmail.com>
2020-03-16 00:40:08 -0500

    added contact form, need to fix unauthorized bug

--- components/Projects.js
+++ components/Projects.js
@@ -8,7 +8,7 @@ export default ({ data }) => {
         <div className="projects">
         <ul className="project-cards">
           {data.slice(0, 20).map((i, index) => (
-            <Fade bottom duration={1500} delay={100 * index} key={i.id}>
+            <Fade bottom duration={1500} delay={10 * index} key={i.id}>
               <li className="project-card">
                 <div className="about">
                   <p>
