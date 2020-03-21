6dcde710e25438b1efed80f07293c04d4a434595
Cameron Roman <camroman95@gmail.com>
2020-03-21 16:24:31 -0500

    started updating about page

--- pages/about.js
+++ pages/about.js
@@ -1,15 +1,46 @@
 import Meta from '../components/Meta'
 import Typography from '@material-ui/core/Typography'
 import Container from '@material-ui/core/Container'
+import Box from '@material-ui/core/Box'
 
 function About() {
     return (
     <div>
         <Meta />
         <Container className="container" align="center">
-            <Typography variant="h2">
-                What will we create?
-            </Typography>
+            <Box p={1}>
+                <Typography variant="h2">
+                    What will we create?
+                </Typography>
+            </Box>
+            <Box p={4}>
+                <Typography variant="body1">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer mollis commodo mi. Sed commodo hendrerit metus vitae ullamcorper. Pellentesque in tortor mollis enim tempus accumsan a sit amet nibh. Duis in lorem ac velit molestie rutrum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum ac nulla volutpat, dapibus velit vel, tempus dolor. Duis vestibulum, nulla non tincidunt tempus, turpis tortor ullamcorper felis, eget aliquet est neque sit amet est. Phasellus bibendum odio nulla, ac bibendum nisl auctor rutrum. Integer blandit id urna non pretium. Donec placerat nulla nec ante maximus consequat.
+
+    Mauris consectetur pretium feugiat. Maecenas hendrerit iaculis justo ut viverra. Donec porttitor, sem vitae dignissim lobortis, ex nisl dictum turpis, nec dignissim sem nisl egestas ligula. Curabitur at ligula vitae risus elementum tincidunt a vitae risus. Pellentesque eleifend, tellus at luctus sollicitudin, nisi tortor fermentum odio, vitae sagittis ligula nisi vitae magna. Sed tincidunt dolor at metus porttitor, ut pretium augue viverra. Suspendisse vitae ipsum nec nisi maximus blandit. Vestibulum eu libero odio. Etiam eget nibh in purus auctor egestas. Aenean gravida vehicula ipsum quis scelerisque. Ut varius, est id accumsan sollicitudin, sem enim sodales turpis, sed hendrerit eros diam et mauris. Aliquam maximus ante odio, sit amet vulputate lorem pretium et.
+
+    Nam pellentesque, purus sed venenatis facilisis, tellus elit aliquam nulla, quis sagittis sapien eros at massa. Fusce a rutrum nunc. Curabitur in vestibulum neque. Duis ac vulputate neque, vel laoreet felis. In euismod mi ut velit pulvinar aliquet. Vestibulum imperdiet, ex quis rutrum scelerisque, enim libero lacinia odio, nec gravida est odio sit amet neque. Vivamus nec malesuada augue. Aenean sed sagittis justo. Donec posuere nec nisl sed feugiat. Proin fringilla commodo est nec aliquet.
+
+    Mauris non enim ex. Suspendisse eget elit vel lacus molestie malesuada id a nisl. Suspendisse erat nulla, mollis sed convallis id, aliquet sed sapien. Proin pharetra orci at hendrerit blandit. Praesent egestas varius bibendum. Donec id vestibulum tellus. Cras vitae diam id magna molestie varius a vel turpis. Sed tristique dictum dui, eget aliquam ligula scelerisque vel. Aliquam posuere maximus nibh a dictum. Fusce porta sit amet est et lobortis. Nullam urna nisl, aliquet nec rutrum non, imperdiet bibendum nisl. Nulla vel efficitur lacus. Cras venenatis dolor in massa dapibus consectetur. Phasellus a dignissim nunc.
+
+    Pellentesque tincidunt sagittis iaculis. In id massa sagittis tellus imperdiet tempor. Ut hendrerit magna vel nulla eleifend pellentesque. Vivamus leo sapien, blandit et velit in, fringilla molestie velit. Aenean non dolor vel metus porttitor porttitor. Nam vehicula nunc ut porttitor sodales. Sed eget sollicitudin diam. Nunc vel lectus nibh. Vestibulum eget viverra erat. Suspendisse sit amet ipsum lacus. Phasellus vitae leo congue, faucibus odio ut, tempus libero. Sed tellus elit, sollicitudin vel suscipit id, dictum eu urna. Praesent ut vestibulum sapien. Sed vel ipsum mollis, luctus neque commodo, egestas orci. Pellentesque pulvinar sapien id ante gravida, eget condimentum est eleifend.
+                </Typography>
+            </Box>
+            <Box p={4}>
+                <Typography variant="body1">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer mollis commodo mi. Sed commodo hendrerit metus vitae ullamcorper. Pellentesque in tortor mollis enim tempus accumsan a sit amet nibh. Duis in lorem ac velit molestie rutrum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum ac nulla volutpat, dapibus velit vel, tempus dolor. Duis vestibulum, nulla non tincidunt tempus, turpis tortor ullamcorper felis, eget aliquet est neque sit amet est. Phasellus bibendum odio nulla, ac bibendum nisl auctor rutrum. Integer blandit id urna non pretium. Donec placerat nulla nec ante maximus consequat.
+
+    Mauris consectetur pretium feugiat. Maecenas hendrerit iaculis justo ut viverra. Donec porttitor, sem vitae dignissim lobortis, ex nisl dictum turpis, nec dignissim sem nisl egestas ligula. Curabitur at ligula vitae risus elementum tincidunt a vitae risus. Pellentesque eleifend, tellus at luctus sollicitudin, nisi tortor fermentum odio, vitae sagittis ligula nisi vitae magna. Sed tincidunt dolor at metus porttitor, ut pretium augue viverra. Suspendisse vitae ipsum nec nisi maximus blandit. Vestibulum eu libero odio. Etiam eget nibh in purus auctor egestas. Aenean gravida vehicula ipsum quis scelerisque. Ut varius, est id accumsan sollicitudin, sem enim sodales turpis, sed hendrerit eros diam et mauris. Aliquam maximus ante odio, sit amet vulputate lorem pretium et.
+
+    Nam pellentesque, purus sed venenatis facilisis, tellus elit aliquam nulla, quis sagittis sapien eros at massa. Fusce a rutrum nunc. Curabitur in vestibulum neque. Duis ac vulputate neque, vel laoreet felis. In euismod mi ut velit pulvinar aliquet. Vestibulum imperdiet, ex quis rutrum scelerisque, enim libero lacinia odio, nec gravida est odio sit amet neque. Vivamus nec malesuada augue. Aenean sed sagittis justo. Donec posuere nec nisl sed feugiat. Proin fringilla commodo est nec aliquet.
+
+    Mauris non enim ex. Suspendisse eget elit vel lacus molestie malesuada id a nisl. Suspendisse erat nulla, mollis sed convallis id, aliquet sed sapien. Proin pharetra orci at hendrerit blandit. Praesent egestas varius bibendum. Donec id vestibulum tellus. Cras vitae diam id magna molestie varius a vel turpis. Sed tristique dictum dui, eget aliquam ligula scelerisque vel. Aliquam posuere maximus nibh a dictum. Fusce porta sit amet est et lobortis. Nullam urna nisl, aliquet nec rutrum non, imperdiet bibendum nisl. Nulla vel efficitur lacus. Cras venenatis dolor in massa dapibus consectetur. Phasellus a dignissim nunc.
+
+    Pellentesque tincidunt sagittis iaculis. In id massa sagittis tellus imperdiet tempor. Ut hendrerit magna vel nulla eleifend pellentesque. Vivamus leo sapien, blandit et velit in, fringilla molestie velit. Aenean non dolor vel metus porttitor porttitor. Nam vehicula nunc ut porttitor sodales. Sed eget sollicitudin diam. Nunc vel lectus nibh. Vestibulum eget viverra erat. Suspendisse sit amet ipsum lacus. Phasellus vitae leo congue, faucibus odio ut, tempus libero. Sed tellus elit, sollicitudin vel suscipit id, dictum eu urna. Praesent ut vestibulum sapien. Sed vel ipsum mollis, luctus neque commodo, egestas orci. Pellentesque pulvinar sapien id ante gravida, eget condimentum est eleifend.
+                </Typography>
+            </Box>
+          
+        
         </Container>
     <style jsx>
         {`
